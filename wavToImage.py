d77db3b1ba8f9aee9c065501a64c7cf1199cd5ff
Nicolas Avrutin <nicolasavru@gmail.com>
2010-08-09 02:18:38 -0400

    overlap

--- wavToImage.py
+++ wavToImage.py
@@ -26,22 +26,28 @@ print sound
 interval = nframes / xres
 print interval, nframes
 
-screen = render.createScreen(xres, interval)#yres)
+screen = render.createScreen(xres, interval/2)#yres)
+
+def generateColor(val):
+    v = math.log(abs(val)+.001)*10
 
 for x in range(xres):
     fft = np.fft.rfft(sound[x*interval:(x+1)*interval])
+    fft = np.fft.rfft(sound[x*interval:(x+1)*interval+10])
     fft = [z.real for z in fft]
-    print len(fft)
+#    print len(fft)
 #    print fft
-#    print "{0}%".format(round(100.0 * x / xres, 2))
+    print "{0}%".format(round(100.0 * x / xres, 2))
     for y in range(interval/2):
 #        p = d[x+xres*y]
-        c = [math.log(abs(fft[y])+.001)*10, 0, 0]#255-math.log(abs(y)+.01), 0]
-        print x, y, c
-        render.plot(x, y, c, screen)
+        c = [math.log(abs(fft[y])+.001)*10,
+             math.log(abs(fft[y])+.001)*10,
+             math.log(abs(fft[y])+.001)*10]
+#        print x, y, c
+        render.plot(xres-x, interval/2-y, c, screen)
 
 #render.display(screen)
-render.saveExtension(screen, "out.png")
+render.saveExtension(screen, "out2.png")
 
 
 
