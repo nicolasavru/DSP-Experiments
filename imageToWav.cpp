266e597df13a25e9fd658863ea6b089a590f80ad
Nicolas Avrutin <nicolasavru@gmail.com>
2010-10-10 01:25:57 -0400

    removed debugging statements

--- imageToWav.cpp
+++ imageToWav.cpp
@@ -13,6 +13,11 @@ g++ imageToWav.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs`
 #include <valarray>
 #include <boost/math/constants/constants.hpp>
 #include <Magick++.h>
+
+#define R 0
+#define G 1
+#define B 2
+
 using namespace std;
 
 const int SAMPLE_RATE = 44100;
@@ -21,14 +26,12 @@ const int BITS_PER_SAMPLE = 16;
 const int YRES = 400;
 const double T_PER_COL = 0.03;
 
-
 double oscillator(double x, double freq=1, double amp=1, double base=0, double phase=0){
     return base + amp * sin(2 * boost::math::constants::pi<double>() * freq * x + phase);
     }
 
 void writewav(string filename, short numChannels, int sampleRate, short bitsPerSample, int nSamples, short * data){
     int dataSize = nSamples * numChannels * bitsPerSample / 8;
-    cout << nSamples << " " << numChannels << " " << bitsPerSample << " " << dataSize << endl;
     string ChunkID = "RIFF";
     int ChunkSize = dataSize + 36;
     string Format = "WAVE";
@@ -67,34 +70,17 @@ int main(int argc, char *argv[]){
     string ARG_IMAGE = argv[2];
     string ARG_OUTFILE = argv[3];
 
-    // string ARG_IMAGE = argv[2];
-    // string ARG_OUTFILE = argv[3];
-    // bool ARG_COLOR = argv[1][0] == 'c' ? true : false;
-    // int CHANNELS = ARG_COLOR ? 3 : 1;
-
-    int CHANNELS = 1;
-    bool ARG_COLOR = false;
-    if(string(argv[1]) == "c"){
-        ARG_COLOR = true;
-        CHANNELS = 3;
-    }
-
-    int R = 0;
-    int G = 1;
-    int B = 2;
+    bool ARG_COLOR = argv[1][0] == 'c' ? true : false;
+    int CHANNELS = ARG_COLOR ? 3 : 1;
 
     Magick::Image im;
     im.read(ARG_IMAGE.c_str());
     int xres = im.size().width();
     int yres = im.size().height();
     double z = double(YRES) / yres;
-    cout << z << endl;
-    cout << xres << " " << yres << endl;
     im.zoom(Magick::Geometry(xres*z, yres*z));
-
     xres = im.size().width();
     yres = im.size().height();
-    cout << xres << " " << yres << endl;
     double yscale = 22000 / (float) yres;
     double sampsPerCol = (int)(SAMPLE_RATE*T_PER_COL);
 
@@ -112,8 +98,7 @@ int main(int argc, char *argv[]){
     }
     out = new short[(int) (CHANNELS * xres * sampsPerCol)];
     for(x = 0; x < xres; x++){
-        cout << x << endl;
-        //        printf("%s: %4.2f\n", ARG_COLOR ? "Color" : "Grayscale", 100.0 * x / xres);
+        printf("%s: %4.2f%%\n", ARG_COLOR ? "Color" : "Grayscale", 100.0 * x / xres);
         for(y = 0; y < yres; y++){
             p[R] = im.pixelColor(x, y).redQuantum()/256;
             p[G] = im.pixelColor(x, y).greenQuantum()/256;
@@ -127,12 +112,8 @@ int main(int argc, char *argv[]){
                     else{
                         amplitude = pow(10, 1-5.25+4.25*(p[R]+p[G]+p[B])/(3*255));
                     }
-                    //                    cout << amplitude << endl;
                     for(i = 0; i < sampsPerCol; i++){
                         tones[c][i] += 1000 * (oscillator(x*T_PER_COL+1 + i*T_PER_COL/sampsPerCol, yscale * (yres - y), amplitude))/l128;
-                        //cout << x*T_PER_COL << " " << i*T_PER_COL + i*T_PER_COL/sampsPerCol << endl;
-                        //cout << (oscillator(i*T_PER_COL + i*T_PER_COL/sampsPerCol, yscale * (yres - y), amplitude))/l128 << endl;
-                        //cout << tones[c][i] << " " << x << " " << y << " " << c << " " << i << " " << amplitude << endl;
                     }
                     //                }
             }
@@ -140,12 +121,8 @@ int main(int argc, char *argv[]){
         for(i = 0; i < sampsPerCol; i++){
             for(c = 0; c < CHANNELS; c++){
                 out[c+CHANNELS*i+(int)sampsPerCol*x] = (short) tones[c][i];
-                //cout << tones[c][i] << " " << (short) tones[c][i] << endl;
                 tones[c][i] = 0;
-                //cout << c << " " << i << " " << x << " " << endl;
-                //cout << c+CHANNELS*i+(int)sampsPerCol*x << " " << out[c+CHANNELS*i+(int)sampsPerCol*x] << endl;
             }
-            //cout << out[(int)sampsPerCol*x] << endl;
         }
     }
 
@@ -153,11 +130,5 @@ int main(int argc, char *argv[]){
     writewav(ARG_OUTFILE, CHANNELS, SAMPLE_RATE, BITS_PER_SAMPLE, (int) xres*sampsPerCol, out);
 
 
-
-    // Magick::Color  pixel_sample;
-    // pixel_sample = im.pixelColor(329, 357);
-    // printf("%u, %u, %u\n", pixel_sample.redQuantum()/(65536/256), pixel_sample.greenQuantum()/(65536/256), pixel_sample.blueQuantum()/(65536/256));
-
-        //    cout << argv[1] << argv[2] << argv[3] << endl;
     return 0;
 }
